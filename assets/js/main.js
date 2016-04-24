e28d77e961e789f6c0339674dd7bb9cd13fa484a
Lisa Ballard <lballard.cat@gmail.com>
2016-04-24 15:12:42 -0700

    slight adjust to floaty site title but...

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,10 @@
-/* adapted from http://jsfiddle.net/qB4Z2/ */
-function sticky_relocate() {
-    var window_top = $(window).scrollTop();
-    var div_top = $('#sticky-anchor').offset().top;
-    if (window_top > div_top) {
-        $('#sticky').addClass('stick');
-        $('#sticky-anchor').height($('#sticky').outerHeight());
-    } else {
-        $('#sticky').removeClass('stick');
-        $('#sticky-anchor').height(0);
-    }
-}
-
 $(function() {
 
     if ($('.project-grid').length) {
+
+        // when scrolling the site title scrolls to top with page and then becomes fixed 
+        $(window).scroll(sh.sticky_relocate);
+        sh.sticky_relocate();
 
         // do any of our homepage grid project titles need smaller font? 
         var min_length = 18;  // if title larger than this then shrink its font
@@ -26,26 +17,27 @@ $(function() {
             }
         }
 
-        // when scrolling the site title scrolls to top with page and then becomes fixed 
-        $(window).scroll(sticky_relocate);
-        sticky_relocate();
     }
 
 });
 
-var dir = 1;
-var MIN_TOP = 200;
-var MAX_TOP = 350;
+var sh = {
 
-function autoscroll() {
-    var window_top = $(window).scrollTop() + dir;
-    if (window_top >= MAX_TOP) {
-        window_top = MAX_TOP;
-        dir = -1;
-    } else if (window_top <= MIN_TOP) {
-        window_top = MIN_TOP;
-        dir = 1;
-    }
-    $(window).scrollTop(window_top);
-    window.setTimeout(autoscroll, 100);
+    /* adapted from http://jsfiddle.net/qB4Z2/ */
+    sticky_relocate: function() {
+        var window_top = $(window).scrollTop();
+        var div_top = $('#sticky-anchor').offset().top;
+        if (window_top > div_top) {
+            // make the top banner stick to top
+            $('#sticky').addClass('stick');
+            $('.hero-image').removeClass('top_margin_adjust');
+            // $('#sticky-anchor').height($('#sticky').outerHeight());
+        } else {
+            // make the top banner go back to it's floaty place
+            $('.hero-image').addClass('top_margin_adjust');
+            $('#sticky').removeClass('stick');
+            // $('#sticky-anchor').height(0);
+        }
+    },
+
 }
