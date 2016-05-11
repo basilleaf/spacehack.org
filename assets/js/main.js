54b9aa0d5a8829ff94c0a54e8b885685be00598f
Lisa Ballard <lballard.cat@gmail.com>
2016-05-10 22:44:25 -0700

    jank reduction redux of scroll-to-fix header for smoother interaction

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,36 +7,89 @@ $(function() {
     window.location.href = $(this).find('a').attr('href');
   });
 
-  // fix the header to the top when it scrolls there
-  $('.header').scrollToFixed({
-      postFixed: function() { 
-
-        $('.tagline').css('margin-top', '-30px'); 
-        $(this).css('background-color', 'transparent'); 
-
-      },
-      preFixed: function() { 
-
-        $('.tagline').css('margin-top', '-10px'); 
-        $(this).css('background-color', 'black'); 
-
-        /*
-        if you want to animate the change of header from transparent to black:
-        $(this).animate({
-          backgroundColor: "black",
-          background: "black",
-        }, 300);
-        */
-
-      }
+  // infinite scroll style lazy loading on homepage 
+  $('.project-grid').jscroll({
+    nextSelector: 'a.next:last',
+    padding:200,
+    callback: function() {
+      $('a.next').remove();  // it was getting confused
+    }
   });
 
-});
+  // handle the sticky header
+  if(sh.isPositionSticky('.header')) {
+    // we can just use css position sticky yay!: just need to handle
+    // extra styling on the header when it is fixed
+    window.setInterval(function() { sh.fixedHeaderstyle(); }, 200);
+
+  } else {
+    // this browser does not suppoer position sticky, 
+    // init scrollToFixed plugin 
+    $('.header').scrollToFixed({
+        postFixed: function() { 
+          $('.tagline').css('margin-top', '200px'); 
+          $(this).css('margin-top', '-200px'); 
+          //$(this).css('background-color', 'transparent'); 
+          $(this).animate({
+            backgroundColor: "transparent",
+          }, 300);
+          dontSetWidth: true;
+          marginTop:0;
+          },
+        preFixed: function() { 
+          $('.tagline').css('margin-top', '0'); 
+          $(this).css('margin-top', 0); 
+          // $(this).css('background-color', 'black'); 
+          $(this).animate({
+            backgroundColor: "black",
+          }, 300);
+          dontSetWidth: true
+          marginTop:0;
+        },
+    }); // scrollToFixed init
+  } // end else position sticky 
+}); // on page load 
+
+
 
 var sh = {
 
-  adjust_grid_fonts: function() {
+  header_is_fixed: false,
 
+  isPositionSticky: function(selector){
+      // does this browser support position: sticky 
+      // via https://github.com/bigspotteddog/ScrollToFixed/issues/100
+      return ($.inArray($(selector).css('position'), ['-webkit-sticky', '-moz-sticky', '-ms-sticky', '-o-sticky', 'sticky']) != -1);
+  }, 
+
+  getScrollTop: function() {
+      scrolltop = $(document).scrollTop();
+      return scrolltop;
+  }, 
+
+  fixedHeaderstyle: function(scrolltop) {
+    // when scroll top is 30px (50px position header minus 20px padding)
+    // turn the background black, else transparent 
+    scrolltop = sh.getScrollTop();
+
+    if (scrolltop > 29) {
+      // page has scrolled so header we infer should be fixed 
+        if (!sh.header_is_fixed) {
+          sh.header_is_fixed = true;        
+          $('header').addClass('black',300);
+        }
+    } else  {
+      // scroll is less than 30, header is released
+        if (sh.header_is_fixed) {
+          sh.header_is_fixed = false;        
+          $('header').removeClass('black');
+        }
+      }
+  },
+
+  adjust_grid_fonts: function() {
+    // some projects have long titles and need a bit smaller
+    // font when displayed in homepage grid circles 
     if ($('.project-grid').length) {
       // do any of our homepage grid project titles need smaller font? 
       var min_length = 18;  // if title larger than this then shrink its font
