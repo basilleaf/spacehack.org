eb9944710ea24c458e2e65306921b0268334fde0
Lisa Ballard <lballard.cat@gmail.com>
2016-09-02 23:56:20 -0700

    resize script now creates social image crop too

--- resize.py
+++ resize.py
@@ -2,9 +2,6 @@
 import PIL.Image as PIL
 import re, os, sys, urlparse
 """
-via https://pandemoniumillusion.wordpress.com/2008/05/04/quick-image-resizing-with-python/
-What:
-Resize all the jpg images in a directory
 
 """
 
@@ -12,14 +9,14 @@ basedir = 'new_images/'
 image_dir = 'assets/img/'
 keyword = 'seti'
 
-all_sizes = {
+all_square_sizes = {
     'xsmall': (450, 450),
     'small': (830, 830),
     'med': (950, 950),
     'large': (1200, 1200),
-    # 'social': (885, 464)
 }
 
+social_size = (885, 464)
 
 try:
     keyword = sys.argv[1]
@@ -34,16 +31,48 @@ found_square = False
 for file in files:
     if JPG.match(file):  # is this a jpeg
         f = image_dir.rstrip("/") + "/" + file
-        if 'square' in f and keyword in f:  # is this the square version
-            found_square = True
-            for path, size in all_sizes.items():
-                filename = f.split('/')[-1]
-                if path == 'social':
-                    filename = filename.replace('_square','_social')
+
+        if keyword in f:  # is this the square version
+
+            # make the square images
+            for path, size in all_square_sizes.items():
+
+                if 'square' in f:
+                    filename = f.split('/')[-1]
+                    found_square = True
+                    img = PIL.open(f)
+                    img.thumbnail(size, PIL.ANTIALIAS)
+                    img.save("assets/img/%s/%s" % (path, filename))
+                    print("created: assets/img/%s/%s at size %s" % (path, filename, str(size)))
+
+            # make the social iages
+            if 'rectangle' in f:  # is this the square version
+                filename = f.split('/')[-1].replace('_rectangle','')
                 img = PIL.open(f)
-                img.thumbnail(size, PIL.ANTIALIAS)
-                print("created: assets/img/%s/%s at %s" % (path, filename, str(size)))
-                img.save("assets/img/%s/%s" % (path, filename))
+                orig_width = img.size[0]
+                orig_height = img.size[1]
+                desired_width = social_size[0]
+                desired_height = social_size[1]
+                if orig_height < desired_height:
+                    print "Could not create social image, rectangle image must be > %s " % str(desired_height)
+                else:
+                    img.thumbnail((orig_width, desired_height), PIL.ANTIALIAS)  # adjust the height to the social size first then we'll crop
+                    width = img.size[0]
+                    height = img.size[1]
+                    half_the_width = width / 2
+                    half_the_height = height / 2
+                    half_desired_width = desired_width / 2
+                    half_desired_height = desired_height / 2
+                    box = (
+                            half_the_width - half_desired_width,
+                            half_the_height - half_desired_height,
+                            half_the_width + half_desired_width,
+                            half_the_height + half_desired_height
+                            )
+
+                    img_social = img.crop(box)
+                    img_social.save("assets/img/social/%s" % filename)
+                    print("created: assets/img/social/%s" % filename)
 
 if not found_square:
     print("could not find a square image for %s" % keyword)
