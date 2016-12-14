521912bacd81d8850da274cbb460747e17550904
Lisa Ballard <lballard.cat@gmail.com>
2016-12-14 13:37:50 -0800

    updates resize script to handle project banner

--- resize.py
+++ resize.py
@@ -1,24 +1,24 @@
 #! /usr/bin/env python
+from shutil import copyfile
 import PIL.Image as PIL
 import re, os, sys, urlparse
 """
 
-  This is the image pipeline for creating responsive image sizes.
-  To add images to the sight, create the square and rectangle images and put them in new_images/
-  then run this script with the project slug as the first argument.
+  This is the an image pipeline for creating responsive image sizes.
+  To add images to the site, create the square and rectangle images and put them in raw_images/
+  then run this script with the project url slug as the first argument.
+
   For exmaple for spacehack.org/seti do this:
+
+  1. create the square (1200x1200) and rectangle (994x298) images in the raw_images/ directory
+
+  2. Run the script like so:
 
     python resize.py seti
 
-  # todo
-  • we are now using same file for social sharing, so reorganize that dir struct
-    and edit any pointers to them (min square should be 1200 x 1200)
-  • make this script also resize (if needed) and copy into right place the rectangle image
-    (large rectangle is 994 x 298)
-
 """
 
-basedir = 'new_images/'
+basedir = 'raw_images/'
 image_dir = 'assets/img/'
 
 all_square_sizes = {
@@ -28,7 +28,7 @@ all_square_sizes = {
     'large': (1200, 1200),
 }
 
-social_size = (1200, 600)
+social_size = (1200, 630)
 
 try:
     keyword = sys.argv[1]
@@ -38,7 +38,7 @@ except:
 
 JPG = re.compile(".*\.(jpg|jpeg)", re.IGNORECASE)
 
-files = os.listdir(image_dir)
+files = os.listdir(basedir)
 found_square = False
 for file in files:
     if JPG.match(file):  # is this a jpeg
@@ -57,15 +57,22 @@ for file in files:
                     img.save("assets/img/%s/%s" % (path, filename))
                     print("created: assets/img/%s/%s at size %s" % (path, filename, str(size)))
 
-            # make the social images for twitter/facebook sharing
+            # deal with the project pages
             if 'rectangle' in f:  # is this the rectangle version
+
+                # move rectangle banner image into correct file location:
+                base_file = f.split('/')[-1]
+                copyfile("%s/%s" % (basedir, base_file), "assets/img/%s" % (base_file))
+                print("created: assets/img/%s" % f.split('/')[-1])
+
+                # make the social images for twitter/facebook sharing
                 filename = f.split('/')[-1].replace('_rectangle','')
                 img = PIL.open(f)
                 orig_width = img.size[0]
                 orig_height = img.size[1]
                 desired_width = social_size[0]
                 desired_height = social_size[1]
-                if orig_height < desired_height:
+                if orig_height <= desired_height:
                     print "Could not create social image, rectangle image must be > %s " % str(desired_height)
                 else:
                     img.thumbnail((orig_width, desired_height), PIL.ANTIALIAS)  # adjust the height to the social size first then we'll crop
